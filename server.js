8ecf10e3507ca7efd2ca4bf6abe89fab2ab4df2d
Chris-Haj <chrishaj1191@gmail.com>
2023-05-28 14:53:38 +0300

    Readded Forum page

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ app.listen(port,()=>{
     console.log(`Starting server on port ${port}`);
 });
 
+//load index page when opening port
 app.use(express.static(__dirname));
 
-app.get('/courses', (req, res) => {
-    // Read the courses.json file and send the data as a response
-    res.sendFile(__dirname + '/courses.json');
+// app.get('/courses', (req, res) => {
+//     // Read the courses.json file and send the data as a response
+//     res.sendFile(__dirname + '/courses.json');
+// });
+
+app.post('/courses',(res,req)=>{
+    console.log(req.params);
 });
+
 
 
