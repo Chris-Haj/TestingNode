e8c26696d9f21ae09f60d1542f32f95c875a66a9
MoeYousef1 <mohammadyousef08@gmail.com>
2023-05-31 00:47:12 +0300

    some changes

--- CoursesServices/ServerSide/routes.js
+++ CoursesServices/ServerSide/routes.js
@@ -1,13 +1,11 @@
-// const express = require('express'),
-//     userRoutes = require('./courses.json');
-//
-// var router = express.Router();
-//
-//
-//
-// router.get('/courses', userRoutes);
-// router.post('/courses', userRoutes);
-// router.put('/courses/:id', userRoutes);
-// router.delete('/courses/:id', userRoutes);
-//
-// module.exports = router;
+const express = require('express'),
+    userRoutes = require('./CRUD.js');
+
+let router = express.Router();
+
+router.get('/courses', userRoutes.getCourses);
+router.post('/courses', userRoutes.CreateCourse);
+router.put('/courses/:id', userRoutes.updateCourse);
+router.delete('/courses/:id', userRoutes.deleteCourse);
+
+module.exports = router;
