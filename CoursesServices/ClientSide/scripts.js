7c4305c7d3ab0bc42bfc7b219a05b090e3a64227
Chris-Haj <chris1191@gmail.com>
2023-05-31 13:16:43 +0300

    View students WIP

--- CoursesServices/ClientSide/scripts.js
+++ CoursesServices/ClientSide/scripts.js
@@ -7,7 +7,8 @@ $(document).ready(() => {
         window.location.href = '/';
     });
 
-    // let form  = $('#user_form');
+
+    let form  = $('#course-form');
     // form.validate({
     //     rules: {
     //         id_field: {
@@ -25,7 +26,7 @@ $(document).ready(() => {
     // })
 
     // Handle form submission
-    let form  = $('#course-form');
+
     form.on('submit', (event) => {
         event.preventDefault();
 
@@ -45,7 +46,8 @@ $(document).ready(() => {
                 lecturer: courseLecturer,
                 start_date: startDate,
                 end_date: endDate,
-                prerequisite_course: prerequisiteCourses
+                prerequisite_course: prerequisiteCourses,
+                students: []
             }
         };
         console.log(course);
@@ -73,28 +75,49 @@ $(document).ready(() => {
         type: 'GET',
         dataType: 'json',
         success: (data) => {
+            let counter = 1;
             const coursesList = $('#courses');
             //get the data from the server and append it to the list
 
             Object.values(data).forEach((course) => {
-                console.log(course);
                 const courseId = course['id'];
                 const courseName = course['name'];
                 const courseLecturer = course['lecturer'];
-                const start = course['start_date'];
-                const end = course['end_date'];
+                const start = course['start_date'].split("-").reverse().join("-");
+                const end = course['end_date'].split("-").reverse().join("-");
 
-                const listItem = $('<li>').text(`Course ID: ${courseId}`);
-                listItem.append((' - '));
-                listItem.append($('<span>').text(`Course Name: ${courseName}`));
-                listItem.append((' - '));
-                listItem.append($('<span>').text(`Lecturer: ${courseLecturer}`));
-                listItem.append($('<br>'));
-                listItem.append($('<span>').text(`Start Date - End Date`));
-                listItem.append(('<br>'));
-                listItem.append($('<span>').text(`${start} - ${end}`));
 
-                coursesList.append(listItem);
+                // Create the course container element
+                const courseItem = $('<li>').prop('id', `course${counter++}`);
+                // Create the course buttons container element
+                const courseButtons = $('<div>').addClass('CourseButtons ChangingButtons');
+                courseButtons.append($('<button>').text('Update Course'));
+                courseButtons.append($('<button>').text('Delete Course'));
+
+                // Create the course details container element
+                const courseDetails = $('<div>').addClass('CourseDetails');
+                courseDetails.append($('<span>').text(`Course ID: ${courseId}`));
+                courseDetails.append($('<span>').text(`Course Name: ${courseName}`));
+                courseDetails.append($('<span>').text(`Lecturer: ${courseLecturer}`));
+                courseDetails.append($('<span>').text('Start Date - End Date'));
+                courseDetails.append($('<span>').text(`${start} - ${end}`));
+
+                // Create the student buttons container element
+                const studentButtons = $('<div>').addClass('StudentButtons ChangingButtons');
+                studentButtons.append($('<button>').text('Add Student').addClass('addStudent'));
+                studentButtons.append($('<button>').text('View Students').addClass('viewStudents'));
+                $('.viewStudents').click((event) => {
+                    console.log('hello');
+
+                    console.log(courseId);
+                });
+                // Append the elements to the course container
+                courseItem.append(courseButtons);
+                courseItem.append(courseDetails);
+                courseItem.append(studentButtons);
+
+                // Append the course container to the courses list
+                coursesList.append(courseItem);
             });
         },
         error: (xhr, status, error) => {
@@ -102,3 +125,4 @@ $(document).ready(() => {
         }
     });
 });
+
